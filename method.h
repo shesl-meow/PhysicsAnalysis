9217776ff96d0eb9bb1b93d80edf268ab72764a9
Chinese-BruceWayne <1184249450@qq.com>
2018-04-12 04:56:56 +0800

    Debug over

--- method.h
+++ method.h
@@ -8,34 +8,38 @@
 	传入值为两个浮点类型指针和整型常量（表示指针指向区域的大小）
 	比如需要测量长宽高计算体积,传入长宽高的计算值和偏差
 */
-_tp_T_ typedef void(*func)(T*,T*,int);
+typedef void(*func)(double*,double*,int);
 
 _tp_T_ void empty(T *val,T *bia,int len){printf("\n");}
-_tp_T_ void method(T **l,const int tm,const int gp, func furtherF = &empty);
+_tp_T_ void method(T **l,const int tm,const int gp,const T ub, func furtherF = &empty);
 
-_tp_T_ void method(
+void method(
 	const int tm,	//重复实验次数
 	const int gp,	//待计算组数
 	char* filename,	//数据所在文件名
 	func furtherF = &empty	//合成不确定度的函数
 	)
 {
-	T l[gp][tm];
-	T ub;
+	double *(l[gp]);
+	double ub;
 	std::fstream infile(filename,std::ios::in);
 	if(!infile.is_open()){ printf("open file failed!"); return; }
-	for(int i=0; i<gp; ++i)
+	for(int i=0; i<gp; ++i){
+		l[i] = new double[tm];
 		for(int j=0; j<tm; ++j)
 			infile>>l[i][j];
+	}
 	infile>>ub;
 	ub /= sqrt(3.0);
 	infile.close();
-	method(l,tm,gp,furtherF);
+	method(l,tm,gp,ub,furtherF);
+	for(int i=0; i<gp; ++i) delete []l[i];
 }
 
 _tp_T_ void method(T **l,	//存储待分析数据的二维数组，每一行表示一个种类的变量
 	const int tm,	//二维数组列数
 	const int gp,	//二维数组行数
+	const T ub,	//B类不确定度
 	func furtherF = &empty	//合成不确定度的函数
 	)
 {
