61eff15f6963b789ac75b3252e49fdbf9edce236
Chinese-BruceWayne <1184249450@qq.com>
2018-04-11 19:07:34 +0800

    modify boom

--- 碰撞/boom.cpp
+++ 碰撞/boom.cpp
@@ -9,16 +9,12 @@ double ee(double v1,double v2,double u1,double u2){
 }
 
 int main(){
-	fstream infile("data.txt",ios::in);
-	if(!infile.is_open()){
-		cerr<<"open wrong!"<<endl;
-		return 0;
-	}
+	std::fstream infile("data.txt",std::ios::in);
+	if(!infile.is_open()) return 0;
 	for(int i=0; i<3; i++)
 		for(int j=0; j<4; j++)
 			infile>>t[i][j];
-	for(int k=0; k<2; k++)
-		infile>>l[k];
+	for(int k=0; k<2; k++) infile>>l[k];
 	infile.close();
 
 	for(int i=0; i<3; i++){
@@ -26,13 +22,12 @@ int main(){
 			printf("%f ",t[i][j]);
 		printf("\n");
 	}
-	for(int k=0; k<2; k++)
-		printf("%f ",l[k]);
+	for(int k=0; k<2; k++) printf("%f ",l[k]);
 
-	cout<<"\n\ne1"<<endl;
+	printf("\n\ncalculate e:\n");
 	for(int i=0; i<3; i++){
 		double v1=l[0]/t[i][0],v2=0,u1=0,u2=l[1]/t[i][1];
-		printf("v1:%f\nu2:%f\n",v1,u2,ee(v1,v2,u1,u2));
+		printf("v1:%f\tu2:%f\te:%f\n",v1,u2,ee(v1,v2,u1,u2));
 	}
 
 	return 0;
