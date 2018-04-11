559e0dff9e4094e1fdee05f0e15674d7081509c2
Chinese-BruceWayne <1184249450@qq.com>
2018-04-12 04:17:36 +0800

    rearrange

--- method.h
+++ method.h
@@ -8,19 +8,20 @@
 	传入值为两个浮点类型指针和整型常量（表示指针指向区域的大小）
 	比如需要测量长宽高计算体积,传入长宽高的计算值和偏差
 */
-typedef void(*func)(double*,double*,int);
+_tp_T_ typedef void(*func)(T*,T*,int);
 
-void empty(double *val,double *bia,int len){printf("\n");}
+_tp_T_ void empty(T *val,T *bia,int len){printf("\n");}
+_tp_T_ void method(T **l,const int tm,const int gp, func furtherF = &empty);
 
-void method(
-	const int tm,			//重复实验次数
-	const int gp,			//待计算组数
-	char* filename,			//数据所在文件名
+_tp_T_ void method(
+	const int tm,	//重复实验次数
+	const int gp,	//待计算组数
+	char* filename,	//数据所在文件名
 	func furtherF = &empty	//合成不确定度的函数
 	)
 {
-	double l[gp][tm];
-	double ub;
+	T l[gp][tm];
+	T ub;
 	std::fstream infile(filename,std::ios::in);
 	if(!infile.is_open()){ printf("open file failed!"); return; }
 	for(int i=0; i<gp; ++i)
@@ -29,8 +30,16 @@ void method(
 	infile>>ub;
 	ub /= sqrt(3.0);
 	infile.close();
+	method(l,tm,gp,furtherF);
+}
 
-	double avg[gp];
+_tp_T_ void method(T **l,	//存储待分析数据的二维数组，每一行表示一个种类的变量
+	const int tm,	//二维数组列数
+	const int gp,	//二维数组行数
+	func furtherF = &empty	//合成不确定度的函数
+	)
+{
+	T avg[gp];
 	printf("average:\n");
 	for(int i=0; i<gp; ++i) avg[i] = mean(l[i],tm), printf("%f\t",avg[i]);
 	printf("\nstantard dev:\n");
@@ -39,7 +48,7 @@ void method(
 	for(int i=0; i<gp; ++i) printf("%f\t",mstan(l[i],tm));
 	
 	//A类不确定度计算
-	double ua[gp];
+	T ua[gp];
 	printf("\nuncertianty(ua):\n");
 	for(int i=0; i<gp; ++i) ua[i] = ucty(l[i],tm), printf("%f\t",ua[i]);
 	
@@ -47,7 +56,7 @@ void method(
 	printf("\nuncertianty(ub):%f",ub);
 
 	//不确定度合成并计算
-	double us[gp];
+	T us[gp];
 	printf("\nuncertianty(synthesis):\n");
 	for(int i=0; i<gp; ++i) us[i] = sqrt( ua[i]*ua[i]+ub*ub ), printf("%f\t", us[i]);
 	
