c5ad81b2566f59fc4fe7024777e78bcb0b9ec283
Chinese-BruceWayne <1184249450@qq.com>
2018-05-09 04:36:32 +0800

    Reduce C++

--- 光栅衍射/biu.cpp
+++ 光栅衍射/biu.cpp
@@ -46,22 +46,21 @@ int main(int argv,char **argc)
 		double lambda[2] =
 			{d*sin(dgr2arc(Half[1]))/2, d*sin(dgr2arc(Half[2]))/2};
 
-		std::cout<<"green wavelength:\t"<<GreenW<<std::endl;
-		std::cout<<"delta angle:\t"<<raw[0][2]<<"\t"<<raw[1][2]<<std::endl;
-		std::cout<<"avrag angle:\t"<<Avg[0]<<std::endl;
-		std::cout<<"half angle:\t"<<Half[0]<<std::endl;
-		std::cout<<"const d:\t"<<d<<std::endl;
+		std::cout<<"green wavelength:\t"<<GreenW<<std::endl
+			<<"delta angle:\t"<<raw[0][2]<<"\t"<<raw[1][2]<<std::endl
+			<<"avrag angle:\t"<<Avg[0]<<std::endl
+			<<"half angle:\t"<<Half[0]<<std::endl
+			<<"const d:\t"<<d<<std::endl;
 
 		std::cout<<"yellow light 1:\t"
-			<<raw[2][2]<<"\t"<<raw[3][2]<<std::endl;
-		std::cout<<"yellow light 2:\t"
-			<<raw[4][2]<<"\t"<<raw[5][2]<<std::endl;
-		std::cout<<"avrag angle 1:\t"<<Avg[1]<<std::endl;
-		std::cout<<"avrag angle 2:\t"<<Avg[2]<<std::endl;
-		std::cout<<"half angle:\t"<<Half[1]<<std::endl;
-		std::cout<<"half angle:\t"<<Half[2]<<std::endl;
-		std::cout<<"wavelength 1:\t"<<lambda[0]<<std::endl;
-		std::cout<<"wavelength 2:\t"<<lambda[1]<<std::endl;
+			<<raw[2][2]<<"\t"<<raw[3][2]<<std::endl
+			<<"yellow light 2:\t"<<raw[4][2]<<"\t"<<raw[5][2]<<std::endl
+			<<"avrag angle 1:\t"<<Avg[1]<<std::endl
+			<<"avrag angle 2:\t"<<Avg[2]<<std::endl
+			<<"half angle:\t"<<Half[1]<<std::endl
+			<<"half angle:\t"<<Half[2]<<std::endl
+			<<"wavelength 1:\t"<<lambda[0]<<std::endl
+			<<"wavelength 2:\t"<<lambda[1]<<std::endl;
 		
 		--argv,++argc;
 	}
